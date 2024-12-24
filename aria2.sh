7d55f6f1b4bafca89a745ca4b107fcac196ad66a
芜湖大司马 <83946612+dasima4396@users.noreply.github.com>
2024-12-24 11:11:43 +0800

    Update aria2.sh

--- aria2.sh
+++ aria2.sh
@@ -88,8 +88,7 @@ Download_aria2() {
 }
 Download_aria2_conf() {
     mkdir -p "${aria2_conf_dir}" && cd "${aria2_conf_dir}"
-    wget -O -t2 -T3 https://github.com/dasima4396/aria2.sh/releases/download/download/aria2.conf.zip
-    unzip aria2.conf.zip
+    wget -O -t2 -T3 https://github.com/dasima4396/aria2.sh/releases/download/download/aria2.conf.zip | unzip
         [[ ! -s ${PROFILE} ]] && {
             echo -e "${Error} '${PROFILE}' 下载失败！清理残留文件..."
             rm -vrf "${aria2_conf_dir}"
